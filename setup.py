137c0115b22204ab87d66b80ac36952a32784a65
Jonas <jonas.scherer@dkfz.de>
2018-02-05 18:10:19 +0100

    update

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+        'requests',
         'pycurl',
         'jsonpickle'
     ],  # Optional
